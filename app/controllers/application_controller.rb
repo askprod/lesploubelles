08abf2a762cae83455038f926300a9f561532802
Paul Trin-Dinh <paultrindinh@icloud.com>
2020-09-29 23:37:44 +0200

    Problem solved for showing the correct dates in the calendar view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :set_json_list
+
+    def set_json_list
+        @json_database = JSON.parse(File.read("app/assets/json/list.json"))
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+
+  match "/map", to: "map#index", via: :get
+  match "/calendar", to: "calendar#index", via: :get
 end
