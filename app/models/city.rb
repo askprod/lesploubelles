090f4b487aaf1d9a6618a455549c5a6119645700
Paul Trin-Dinh <paultrindinh@icloud.com>
2020-10-02 13:41:30 +0200

    Starting to implement elestic search

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,15 @@
 class City < ApplicationRecord
     extend FriendlyId
     friendly_id :name, use: :slugged
+    searchkick word_start: [:name]
+
+    def search_data
+        {
+            name: name
+        }
+    end
+    
+    City.reindex
     
     has_many :sectors
     has_many :streets, through: :sectors
