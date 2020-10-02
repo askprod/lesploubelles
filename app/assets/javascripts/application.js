4e654d4d99f1dafaf826d20417f32859a4a43276
Paul Trin-Dinh <paultrindinh@icloud.com>
2020-10-02 12:59:02 +0200

    Added maps functionnality for each city and each street

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,30 @@
 //
 //= require jquery3
 //= require popper
-//= require bootstrap
+//= require bootstrap-sprockets
 //= require cocoon
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+function initMap(inputAddress, mapOptions) {
+    const map = new google.maps.Map(document.getElementById('city-map'), mapOptions);
+
+    const geocoder = new google.maps.Geocoder();
+    geocodeAddress(inputAddress, map);
+
+    function geocodeAddress(address, resultsMap) {
+        geocoder.geocode({ address: address }, (results, status) => {
+          if (status === "OK") {
+            resultsMap.setCenter(results[0].geometry.location);
+            new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location,
+            });
+          } else {
+            alert("Geocode was not successful for the following reason: " + status);
+          }
+        });
+      }
+}
